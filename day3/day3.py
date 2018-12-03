81e8b57e740b87b51486143913170ae4429d2871
Phil Richman <philip.richman@udemy.com>
2018-12-03 13:49:01 +0000

    Don't bother recreating each array

--- day3/day3.py
+++ day3/day3.py
@@ -13,22 +13,10 @@ with open(input_file_name, 'r') as file:
 
 areas = puzzle_input.strip().split('\n')
 
+area_arrays = {}
+
 num_overlaps = 0
 overlap_array = np.zeros((1000, 1000), dtype=int)
-for area in areas:
-    _, _, coords, size = area.split()
-    x, y = map(int, coords.strip(':').split(','))
-    w, h = map(int, size.split('x'))
-
-    area_array = np.zeros((1000, 1000), dtype=int)
-    area_array[y:y + h, x:x + w] = 1
-
-    overlap_array += area_array
-
-overlap_mask = overlap_array > 1
-
-print('Number of overlapping squares', np.sum(overlap_mask))
-
 for area in areas:
     area_id, _, coords, size = area.split()
     x, y = map(int, coords.strip(':').split(','))
@@ -36,7 +24,15 @@ for area in areas:
 
     area_array = np.zeros((1000, 1000), dtype=int)
     area_array[y:y + h, x:x + w] = 1
+    area_arrays[area_id] = area_array
 
+    overlap_array += area_array
+
+overlap_mask = overlap_array > 1
+
+print('Number of overlapping squares', np.sum(overlap_mask))
+
+for area_id, area_array in area_arrays.items():
     if not np.any(area_array * overlap_mask):
         print('Non overlapping ID', area_id)
         break
