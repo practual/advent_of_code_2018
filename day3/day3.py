21711b1fe86db7e782b9d0ecf08add473decc98f
Phil Richman <philip.richman@udemy.com>
2018-12-03 13:07:57 +0000

    Day 3

---
+++ day3/day3.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+# http://adventofcode.com/2018/day/3
+
+import sys
+
+import numpy as np
+
+input_file_name = sys.argv[1]
+
+with open(input_file_name, 'r') as file:
+    puzzle_input = file.read()
+
+areas = puzzle_input.strip().split('\n')
+
+num_overlaps = 0
+overlap_array = np.zeros((1000, 1000), dtype=int)
+for area in areas:
+    _, _, coords, size = area.split()
+    x, y = map(int, coords.strip(':').split(','))
+    w, h = map(int, size.split('x'))
+
+    area_array = np.zeros((1000, 1000), dtype=int)
+    area_array[y:y + h, x:x + w] = 1
+
+    overlap_array += area_array
+
+overlap_mask = overlap_array > 1
+
+print('Number of overlapping squares', np.sum(overlap_mask))
+
+for area in areas:
+    area_id, _, coords, size = area.split()
+    x, y = map(int, coords.strip(':').split(','))
+    w, h = map(int, size.split('x'))
+
+    area_array = np.zeros((1000, 1000), dtype=int)
+    area_array[y:y + h, x:x + w] = 1
+
+    if not np.any(area_array * overlap_mask):
+        print('Non overlapping ID', area_id)
+        break
