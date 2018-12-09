0f85f95c8242126c2d264376cf06d70e72d4fb6a
Phil Richman <philip.richman@udemy.com>
2018-12-09 20:18:41 +0000

    Day 9

---
+++ day9/day9.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+# http://adventofcode.com/2018/day/9
+
+from collections import defaultdict, deque
+
+
+def find_high_score(num_players, max_marble_value):
+    marbles = deque([0])
+    player_points = defaultdict(int)
+
+    current_player = 1
+    for marble in range(1, max_marble_value + 1):
+        if marble % 23:
+            marbles.rotate(-1)
+            marbles.append(marble)
+        else:
+            player_points[current_player] += marble
+            marbles.rotate(7)
+            player_points[current_player] += marbles.pop()
+            marbles.rotate(-1)
+        current_player += 1
+        current_player = current_player % num_players
+
+    return max(player_points.values())
+
+
+print(find_high_score(476, 71431))
+print(find_high_score(476, 7143100))
